2ac89bcae537d272bdeff16ba6abbfe2695d52ca
KayKay-git <kayalew@yahoo.com>
2020-12-14 18:54:13 -0500

    Implemented listPets function, tests pass

--- src/adaPets.js
+++ src/adaPets.js
@@ -10,6 +10,13 @@ const BASE_URL = 'http://localhost:3000/pets/';
 // Option functions.
 const listPets = () => {
   // Fill out as part of Wave 1.
+  axios.get(BASE_URL)
+  .then((response) => {
+    setResult(response.data);
+  })
+  .catch((error) => {
+    setError('Failure!');
+  });
 };
 
 const showDetails = (selectedPetId) => {
