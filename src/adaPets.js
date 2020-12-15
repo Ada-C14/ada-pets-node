a77cf914f6dea26c4d2eb9fc02b36c8f92104e83
Beatrice T <be@trice.codes>
2020-12-14 20:17:44 -0800

    Wrote showDetails function

--- src/adaPets.js
+++ src/adaPets.js
@@ -23,8 +23,16 @@ const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
   } else {
-    // Fill out as part of Wave 2.
-  }
+    // console.log(BASE_URL + `${selectedPetId}`);
+    axios.get(BASE_URL + `${selectedPetId}`)
+      .then(response => {
+        setResult(response.data);
+        // console.log(response.data);
+      })
+      .catch(error => {
+        setError(`${error.response.status} Failed to retrieve pet`);
+      })
+  };
 };
 
 const removePet = (selectedPetId) => {
