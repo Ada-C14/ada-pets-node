87ca14bd962224b016bf9e4cbb3f52f7e4c2334b
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-12-14 22:49:12 -0800

    implement removePet

--- src/adaPets.js
+++ src/adaPets.js
@@ -36,7 +36,13 @@ const removePet = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to remove a pet without selecting it!");
   } else {
-    // Fill out as part of Wave 3.
+    axios.delete(`${BASE_URL}${selectedPetId}`)
+      .then((response) => {
+        setResult(`Successfully removed pet with id ${selectedPetId}`);
+      })
+      .catch((error) => {
+        setError(`Failed to remove pet. Error status: ${error.response.status}`);
+      })
   }
 };
 
