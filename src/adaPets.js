de46be614d2acd04f68a53f4dd9bd319e8939d1e
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-14 22:47:00 -0800

    added method for showDetails and passed the tests

--- src/adaPets.js
+++ src/adaPets.js
@@ -24,7 +24,16 @@ const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
   } else {
-    // Fill out as part of Wave 2.
+    const axios = require('axios');
+
+    axios.get(BASE_URL+`${selectedPetId}`)
+      .then((response) => {
+        return setResult(response.data)
+      })
+      .catch((error) => {
+        setError('failed, 404')
+        return setError(error.message)
+      });
   }
 };
 
