a0a8cb6a04544cc7f237c29311cbf9bb659bf302
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-12-14 23:37:24 -0800

    Wave 3 tests passing

--- src/adaPets.js
+++ src/adaPets.js
@@ -38,6 +38,14 @@ const removePet = (selectedPetId) => {
     setError("You tried to remove a pet without selecting it!");
   } else {
     // Fill out as part of Wave 3.
+    axios.delete(`http://localhost:3000/pets/${selectedPetId}`)
+    .then((response) => {
+      setResult(response.data);
+
+    })
+    .catch((error) => {
+      setError(`Yikes! Failed to remove pet with the selected pet id of: ${selectedPetId}`);
+    })
   }
 };
 
