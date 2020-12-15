1c556e03b55a446f0f820e42960fd23dacc0fba4
Hanh Solo <minhhanhnyc@gmail.com>
2020-12-15 10:26:51 -0800

    completed setup

--- src/adaPets.js
+++ src/adaPets.js
@@ -10,7 +10,23 @@ const BASE_URL = 'http://localhost:3000/pets/';
 // Option functions.
 const listPets = () => {
   // Fill out as part of Wave 1.
-};
+  axios.get(BASE_URL, {
+    params: {
+      id: id,
+      name: '',
+    }
+  })
+  .then((result) => {
+    
+    
+  })
+  .catch((error)=> {
+
+  })
+
+  petList = []
+
+}
 
 const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
