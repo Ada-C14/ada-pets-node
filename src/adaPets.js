b9ebfbac39cd9ab8048956e0b3dd21fc385f78dc
Lina Do <l.do5147@gmail.com>
2020-12-15 14:55:35 -0800

    finished wave 1

--- src/adaPets.js
+++ src/adaPets.js
@@ -10,6 +10,17 @@ const BASE_URL = 'http://localhost:3000/pets/';
 // Option functions.
 const listPets = () => {
   // Fill out as part of Wave 1.
+
+  axios.get(BASE_URL)
+    .then((response) => {
+      setResult(response.data);
+    })
+    .catch((error) => {
+      setError('Something went wrong. Unable to get list of pets!')
+    }
+    )
+
+
 };
 
 const showDetails = (selectedPetId) => {
