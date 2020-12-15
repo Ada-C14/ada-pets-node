7af2833ef6323fc5cf168a1e62cbc2b1b5be799e
Ren Carothers <rencarothers@gmail.com>
2020-12-14 18:00:28 -0800

    completes showDetails, wave2

--- src/adaPets.js
+++ src/adaPets.js
@@ -25,6 +25,13 @@ const showDetails = (selectedPetId) => {
     setError("You tried to show details for a pet without selecting it!");
   } else {
     // Fill out as part of Wave 2.
+    axios.get(`${BASE_URL + selectedPetId}`)
+    .then((response) => {
+      setResult(response.data);
+    })
+    .catch((error) => {
+      setError(error.message);
+  });
   }
 };
 
