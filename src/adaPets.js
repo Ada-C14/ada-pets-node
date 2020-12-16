1146cae9157564f4be308da947d156df5e4cf4d7
marks214 <aimee.oz.x@gmail.com>
2020-12-15 18:57:26 -0800

    wave 3 tests passing

--- src/adaPets.js
+++ src/adaPets.js
@@ -38,7 +38,14 @@ const removePet = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to remove a pet without selecting it!");
   } else {
-    // Fill out as part of Wave 3.
+    // Wave 3
+    axios.delete(BASE_URL + selectedPetId)
+    .then((response) => {
+      setResult(`Pet with id ${selectedPetId} has been adopted ❤️❤️❤️!`);
+    })
+    .catch((error) => {
+      setError('failed to remove pet: something went wrong 👾🧨');
+    })
   }
 };
 
