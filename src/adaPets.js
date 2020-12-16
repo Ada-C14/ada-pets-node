d22b89038fdeaf00740487d6c61ed850696836b0
Ringo Alcock <ringo.alcock@gmail.com>
2020-12-16 11:18:10 -0800

    define showDetails function

--- src/adaPets.js
+++ src/adaPets.js
@@ -44,7 +44,13 @@ const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
   } else {
-    // Fill out as part of Wave 2.
+    axios.get(BASE_URL + selectedPetId)
+      .then((response) => {
+        setResult(response.data);
+      })
+      .catch((error) => {
+        setError('sorry, your request failed - 404 error');
+      })
   }
 };
 
