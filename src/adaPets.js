716580013eb8d176c7cd77051e3487f6e4e24d4e
Lina Do <l.do5147@gmail.com>
2020-12-15 16:59:47 -0800

    finished wave 4 and passed tests

--- src/adaPets.js
+++ src/adaPets.js
@@ -54,6 +54,19 @@ const removePet = (selectedPetId) => {
 
 const addPet = (petInfo) => {
   // Fill out as part of Wave 4.
+  
+  const petData = {
+    name: petInfo.name,
+    ...petInfo.options
+  }
+  
+  axios.post(BASE_URL, petData)
+  .then((response) => {
+    setResult(response.data);
+  })
+  .catch((error) => {
+    setError('Failed to add a new pet!');
+  });
 };
 
 // Use Node-style exports to export functions for tests and main.
