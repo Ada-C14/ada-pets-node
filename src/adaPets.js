6ce8992e70d86ea33d1b2028d9784297825cccfe
gessica <gessicamohr18@gmail.com>
2020-12-16 11:44:25 -0700

    wave 2 complete with passing tests

--- src/adaPets.js
+++ src/adaPets.js
@@ -24,6 +24,13 @@ const showDetails = (selectedPetId) => {
     setError("You tried to show details for a pet without selecting it!");
   } else {
     // Fill out as part of Wave 2.
+    axios.get(`${BASE_URL}${selectedPetId}`)
+    .then((response) => {
+      setResult(response.data);
+    })
+    .catch((error) => {
+      setError('404 error, failed to get pet information')
+    })
   }
 };
 
