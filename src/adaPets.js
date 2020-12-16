4db6ded31d7a0ce7714fc3ff191444ac00af9a93
Mackenzie <mvlofthus@gmail.com>
2020-12-15 21:11:06 -0800

    initial set up, wave 1 tests pass - cannot get postman to POST, PUT, or PATCH correctly. DELETE and GET work just fine

--- src/adaPets.js
+++ src/adaPets.js
@@ -10,6 +10,15 @@ const BASE_URL = 'http://localhost:3000/pets/';
 // Option functions.
 const listPets = () => {
   // Fill out as part of Wave 1.
+  axios.get(BASE_URL)
+    .then((response) => {
+      // console.log(response.data);
+      setResult(response.data);
+    })
+    .catch((error) => {
+      // console.log(error.response);
+      setError('There was an error!');
+    })
 };
 
 const showDetails = (selectedPetId) => {
