006cb75b614008c10d4f0dcc8175268da277f44d
Sandy <sandyvasquez9@gmail.com>
2020-12-16 23:38:12 -0800

    wave 1, 2, 3 completed. Wave 4 almost completed

--- src/adaPets.js
+++ src/adaPets.js
@@ -9,14 +9,29 @@ const BASE_URL = 'http://localhost:3000/pets/';
 
 // Option functions.
 const listPets = () => {
-  // Fill out as part of Wave 1.
+  axios.get(BASE_URL)
+  .then((response) => {
+    setResult(response.data)
+  })
+  .catch((error) => {
+    setError("invalid url")
+  })
 };
 
 const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
   } else {
+
     // Fill out as part of Wave 2.
+    //where is petDetails function
+    axios.get(BASE_URL + selectedPetId)  //need to call PETS API
+    .then((response) => {
+      setResult(response.data)
+    })
+    .catch((error) => {
+      setError(` ${ error }`)
+    })
   }
 };
 
@@ -25,11 +40,26 @@ const removePet = (selectedPetId) => {
     setError("You tried to remove a pet without selecting it!");
   } else {
     // Fill out as part of Wave 3.
+    
+    axios.delete(BASE_URL + selectedPetId)
+    .then((response) => {
+      setResult(`Success! Deleted ${selectedPetId}`)
+    })
+    .catch((error) => {
+      setError(` Failed to remove ${ error }`)
+    })
   }
 };
 
 const addPet = (petInfo) => {
   // Fill out as part of Wave 4.
+  axios.post(BASE_URL + petInfo)
+  .then((response) => {
+    setResult(`Success! ${petInfo}`)
+  })
+  .catch((error) => {
+    setError(` Failed to add ${ error }`)
+  })
 };
 
 // Use Node-style exports to export functions for tests and main.
