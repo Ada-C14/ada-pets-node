b7bc0d81f1c8a767537001a438e514e20368e372
dnsrocha <drocha@gmail.com>
2020-12-16 22:53:00 -0800

    refactor code

--- src/adaPets.js
+++ src/adaPets.js
@@ -20,13 +20,9 @@ const showDetails = (selectedPetId) => {
     setError("You tried to show details for a pet without selecting it!");
   } else {
     // Fill out as part of Wave 2.
-    axios.get(BASE_URL + selectedPetId)
-    .then((response) => {
-      setResult(response.data);
-    })
-    .catch((error) => {
-      setError(`${error.response.status}: failed request`);
-    });
+  axios.get(BASE_URL + selectedPetId)
+    .then((result) => {setResult(result.data)})
+    .catch((error) => {setError(`${error.response.status}: failed request`)})
   }
 };
 
