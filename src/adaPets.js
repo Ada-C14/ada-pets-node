334083888350449666b050db4ee2f1f82449fd3f
Ayesha Asif <ayaseef@gmail.com>
2020-12-17 11:38:19 -0800

    Added showDetails functionality

--- src/adaPets.js
+++ src/adaPets.js
@@ -22,8 +22,14 @@ const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
   } else {
-    // Fill out as part of Wave 2.
-  }
+    axios.get(`${BASE_URL}${selectedPetId}`)
+      .then((response) => {
+        setResult(response.data)
+      })
+      .catch((error) => {
+        setError('Error: Request failed with status code 404')
+      })
+  };
 };
 
 const removePet = (selectedPetId) => {
