6b08153ef818dd1f9cc628bc9e98df74dd08ea50
Sophia Donan <sophiadonan@gmail.com>
2020-12-17 03:27:44 -0800

    Wave2: complete and all tests passing

--- src/adaPets.js
+++ src/adaPets.js
@@ -20,7 +20,15 @@ const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
   } else {
-    // Fill out as part of Wave 2.
+    axios.get(`${BASE_URL}${selectedPetId}`)
+    
+    .then((response) => {
+      setResult(response.data);
+      })
+    .catch((error) => {
+      setError(`${selectedPetId} failed with status code: ${error.response.status}`);
+    });  
+
   }
 };
 
